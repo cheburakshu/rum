11b9287315f141397a77335bce8cfc45d8785f58
AparnaRajagopalanSud <Aparna&Sud>
2018-05-24 22:55:44 -0600

    incr change

--- rum.js
+++ rum.js
@@ -2,95 +2,115 @@
 /**
     Variable declarations
 */
-    var isServer = false, hasServiceWorker = false, isServiceWorkerInstalled = false;
+    var isNode = false, hasServiceWorker = false, isServiceWorkerInstalled = false;
 
 /**
     Check to see if rum is running on a server or a client. All checks should be here.
 */
-    isServer = (typeof module !== "undefined" && module !== null) && !(typeof window !== "undefined" && window !== null ? window.module : void 0);
-	hasServiceWorker = 'serviceWorker' in navigator
-	hasBeacon = 'sendBeacon' in navigator
-    hasFetch = self.fetch !== undefined;
+    isNode = (typeof module !== "undefined" && module !== null) && !(typeof window !== "undefined" && window !== null ? window.module : void 0);
+    hasServiceWorker = typeof navigator !== "undefined" && 'serviceWorker' in navigator
+    hasBeacon = typeof navigator !== "undefined" && 'sendBeacon' in navigator
+    hasFetch = typeof self !== "undefined" && self.fetch !== undefined;
 
+
+/*
+    Global callables based on server/client
+*/
+    var performance, performanceObserver
+    if (isNode) {
+        var {performance, PerformanceObserver} = require('perf_hooks');
+    } else {
+        var PerformanceObserver = self.PerformanceObserver 
+	var performance = self.performance
+    }
+        
 /**
     Register a service worker if the client supports it. This will handle all of aggregation, analytics and telemetry.
 */
-    if (!isServer && hasServiceWorker) {
-	    navigator.serviceWorker.register('/worker.js')
-		.then(function(){
-		    isServiceWorkerInstalled = true;
-		    console.log('Service worker sucessfully registered!');
-		})
-		.catch(function(err){
-		    isServiceWorkerInstalled = false;
-		    console.error('Service worker registration failed :(', err);
-		})
-	}
+    if (!isNode && hasServiceWorker) {
+        navigator.serviceWorker.register('/worker.js')
+        .then(function(){
+            isServiceWorkerInstalled = true;
+            console.log('Service worker sucessfully registered!');
+        })
+        .catch(function(err){
+            isServiceWorkerInstalled = false;
+            console.error('Service worker registration failed :(', err);
+        })
+    }
 
 /* Performance API functions [W3C standard]*/
-    var allTypes = {'entryTypes': ['mark', 'longtask', 'frame', 'navigation', 'resource']}
-	function getObserver(callback){
-	    var observer = new PerformanceObserver(callback)
-		return observer
-	}
-	
-	function observe(observer, entryTypes){
-	    if (entryTypes === undefined || !entryTypes.hasOwnProperty('entryTypes')){
-		    entryTypes = allTypes
-		}
-	    observer.observe(entryTypes)
-	}
+    var allTypes = {'entryTypes': ['mark', 'longtask', 'frame', 'navigation', 'resource']},
+        perfQueue = []
+
+    function getObserver(callback){
+        var observer = new PerformanceObserver(callback)
+        return observer
+    }
     
-	function snapshot(entryTypes){
-	    if (entryTypes === undefined || !entryTypes.hasOwnProperty('entryTypes') || !entryTypes['entryTypes'] instanceof Array){
-		    entryTypes = allTypes
-		}
-		for (var idx in entryTypes['entryTypes']){
-   	        var perfEntries = performance.getEntriesByType(entryTypes['entryTypes'][idx])    
-		    for (var i = 0; i < perfEntries.length; i++){
-		        console.table(perfEntries[i])
-		    }
-		}
-	}
-	
-	var sendToConsole = function(list){
-	    var perfEntries = list.getEntries();
-		for (var i = 0; i < perfEntries.length; i++){
-		    console.table(perfEntries[i])
-		}
-	}
+    function observe(observer, entryTypes){
+        if (entryTypes === undefined || !entryTypes.hasOwnProperty('entryTypes')){
+            entryTypes = allTypes
+        }
+        observer.observe(entryTypes)
+    }
+    
+    function snapshot(entryTypes){
+        if (entryTypes === undefined || !entryTypes.hasOwnProperty('entryTypes') || !entryTypes['entryTypes'] instanceof Array){
+            entryTypes = allTypes
+        }
+        for (var idx in entryTypes['entryTypes']){
+            var perfEntries = performance.getEntriesByType(entryTypes['entryTypes'][idx])    
+	    pushToQueue(perfEntries)
+        }
+    }
+    
+    function pushToQueue(list){
+        for (var i = 0; i < list.length; i++){
+            perfQueue.push(list[i])
+        }
+    }
+
+    function send(){
+        console.log(perfQueue.shift())
+    }
+
+    var observerCallback = function(list){
+        var perfEntries = list.getEntries();
+	pushToQueue(perfEntries)
+    }
 /**
     Program logic is encapsulated in exportDef which is exposed as a function
 */
     exportDef = function() {
-	    var hello, options;
-	    hello = function() {
-		    console.log('Hello, World!');
-		}
-		
-		/*
-		Configurations for Rum accepted as a dictionary.
-		*/
-		config = function(options) {
-		    options = Object.assign({},options)
-		}
-		
-		print = function(){
-		    var observer = getObserver(sendToConsole)
-			observe(observer, {})
-		}
-		
-		/*
-		Export functions for call
-		*/
-		exports = {
-		    hello: hello,
-			config: config,
-			print: print,
-			snapshot: snapshot
-		};
-		return exports
-	}
+        var hello, options;
+        hello = function() {
+            console.log('Hello, World!');
+        }
+        
+        /*
+        Configurations for Rum accepted as a dictionary.
+        */
+        config = function(options) {
+            options = Object.assign({},options)
+        }
+        
+        print = function(){
+            var observer = getObserver(observerCallback)
+            observe(observer, {})
+        }
+        
+        /*
+        Export functions for call
+        */
+        exports = {
+            hello: hello,
+            config: config,
+            print: print,
+            snapshot: snapshot
+        };
+        return exports
+    }
 /** 
     Make Rum available globally for imports and usage
 */
@@ -102,3 +122,4 @@
       window.Rum = exportDef();
     }
 }).call(this)
+
